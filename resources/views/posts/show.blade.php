137cd14fc3daca7d992bfeaad70cf17eb7de2283
ipkolol <dubravec.filip@gmail.com>
2021-01-17 16:52:11 +0100

    Commit 10. summernote text editor added to post.show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- include libraries(jQuery, bootstrap) -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    </head>
+
     <div class="container">
         <div class="row no-gutters">
             <div class="col-sm-12" >
@@ -19,5 +31,30 @@
         </div>
         <hr>
         <h2>Comments: </h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <form method=”POST” action="">
+                            @csrf
+                            <div class="form-group">
+                                <textarea class="form-control" name="summernote" id="summernote"></textarea>
+                            </div>
+                            <button type=”submit” class="btn btn-success btn-block">Add comment</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
     </div>
+
+    <!-- summernote css/js -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+    <script type="text/javascript">
+        $('#summernote').summernote({
+            height: 100
+        });
+    </script>
 @endsection
